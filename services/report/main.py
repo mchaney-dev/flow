63cf7ca8ef10e724c824c85a09e39be3d715fcc2
Max Haney <60326999+mchaney-dev@users.noreply.github.com>
2025-05-07 12:11:29 -0400

    implemented report service

--- services/report/main.py
+++ services/report/main.py
@@ -1,6 +1,7 @@
 from google.cloud import firestore
 import json
 import logging
+import re
 
 STATUS = {
     200: "OK",
@@ -62,12 +63,6 @@ def request_handler(request):
                     # get a report
                     case "GET":
                         return get_report(report_id)
-                    # update a report
-                    case "PATCH":
-                        return update_report(report_id, data)
-                    # delete a report
-                    case "DELETE":
-                        return delete_report(report_id)
                     # handle invalid request method
                     case _:
                         logging.error(f"Invalid request method: {request.method}")
@@ -108,24 +103,168 @@ def http_response(status: int, data=None):
 
 # GET /reports
 def get_reports(query_params=None):
-    pass
+    reports = get_reports_collection()
+    query = reports
+    docs = []
+
+    # query params not implemented yet
+    if query_params:
+        return NotImplementedError()
+    
+    try:
+        docs = list(query.stream())
+        query_result = [doc.to_dict() for doc in docs]
+
+        data = {
+            "reports": query_result
+        }
+
+        return http_response(200, data)
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+        return http_response(500)
 
 # DELETE /reports
 def delete_reports(query_params=None):
-    pass
+    try:
+        db = get_db()
+        reports = get_reports_collection()
+        query = reports
+
+        # query params not implemented yet
+        if query_params:
+            return NotImplementedError()
+        
+        docs = list(query.stream())
+
+        batch = db.batch()
+        deleted_ids = []
+
+        for i, doc in enumerate(docs):
+            batch.delete(doc.reference)
+            deleted_ids.append(doc.id)
+
+            # commit every 500 deletes for batch deleting
+            if (i + 1) % 500 == 0:
+                batch.commit()
+                batch = db.batch()
+        # commit remaining deletes
+        if len(docs) % 500 != 0:
+            batch.commit()
+        
+        data = {
+            "deletedReportCount": len(deleted_ids),
+            "deletedReportIds": deleted_ids
+        }
+
+        return http_response(200, data)
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+        return http_response(500)
 
 # POST /reports
 def create_report(data):
-    pass
+    try:
+        reports = get_reports_collection()
+        query = reports
+        report_types = [
+            "delay",
+            "no-show",
+            "early departure",
+            "route change",
+            "overcrowding",
+            "missed stop",
+            "accessibility issues"
+        ]
+
+        # account for missing fields
+        if not data.get("type"):
+            logging.error(f"Missing 'type' in request body")
+            return http_response(400)
+        if not data.get("route"):
+            logging.error(f"Missing 'route' in request body")
+            return http_response(400)
+        if not data.get("timestamp"):
+            # TODO: automatically generate timestamp
+            logging.error(f"Missing 'timestamp' in request body")
+            return http_response(400)
+        if not data.get("createdBy"):
+            # TODO: automatically generate createdBy
+            logging.error(f"Missing 'createdBy' in request body")
+            return http_response(400)
+
+        # invalid report type
+        if data["type"] not in report_types:
+            logging.error(f"Invalid report type: {data['type']}")
+            return http_response(400)
+        
+        # normalize report type
+        report_type = data["type"].strip()
+        report_type = re.sub(r"\s+", " ", report_type)
+        report_type = report_type.title()
+
+        # normalize route name
+        route = data["route"].strip()
+        route = re.sub(r"\s+", " ", route)
+        route = route.title()
+
+        # normalize stop name
+        if data.get("stop"):
+            if not isinstance(data["stop"], str) or data["stop"] == "":
+                logging.error(f"Invalid stop type: {type(data['stop'])}, must be string")
+                return http_response(400)
+            stop = data["stop"].strip()
+            stop = re.sub(r"\s+", " ", stop)
+            stop = stop.title()
+            # invalid stop
+            if re.match(r"^[A-Za-z0-9\s-]+$", stop) is None:
+                logging.error(f"Invalid stop name: {stop}. Cannot contain special characters or underscores.")
+                return http_response(400)
+
+        # invalid route
+        if re.match(r"^[A-Za-z0-9\s-]+$", route) is None:
+            logging.error(f"Invalid route name: {route}. Cannot contain special characters or underscores.")
+            return http_response(400)
+        
+        # create new report
+        doc = reports.document()
+        report_id = doc.id
+        # id is automatically generated
+        data["id"] = report_id
+        data["route"] = route
+        if data.get("stop"):
+            data["stop"] = stop
+        data["timestamp"] = "test_timestamp"
+        # TODO: generate createdBy with authentication
+        data["createdBy"] = "test_user_id"
+        doc.set(data)
+
+        return http_response(201)
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+        return http_response(500)
 
 # GET /reports/{id}
 def get_report(report_id):
-    pass
+    reports = get_reports_collection()
+    query = reports
+    docs = []
+    
+    try:
+        docs = list(query.where("id", "==", report_id).stream())
+        if not docs:
+            logging.error(f"Report with ID {report_id} not found")
+            return http_response(404)
+        # check if more than one report with the same ID exists
+        if len(docs) > 1:
+            logging.error(f"Multiple reports with ID {report_id} found")
+            return http_response(500)
+        
+        data = {
+            "report": docs[0].to_dict()
+        }
 
-# PATCH /reports/{id}
-def update_report(report_id, data):
-    pass
-
-# DELETE /report/{id}
-def delete_report(report_id):
-    pass
+        return http_response(200, data)
+    except Exception as e:
+        logging.error(f"Internal server error: {e}")
+        return http_response(500)
